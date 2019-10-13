9e47c82687ec5f70fa77ed4ea0436b1bd8428497
Adnan Haideri <ahaideri@iu.edu>
2019-10-12 20:29:32 -0400

    added new function setActivePersons

--- app/public/js/personRecords.app.js
+++ app/public/js/personRecords.app.js
@@ -2,15 +2,25 @@ var personRecordsApp = new Vue({
   el: '#middle',
   data: {
     persons: [],
+    activePersons: []
   },
   methods: {
-    fetchPatients() {
+    fetchPersons() {
       fetch('api/records/')
       .then(response => response.json())
-      .then(json => { personRecordsApp.persons = json })
+      .then(json => { personRecordsApp.persons = json });
+    },
+    setActivePersons(){
+      console.log(this.persons.length);
+      for(var person in this.persons){
+        if(person.isActive == 1){
+          console.log(person);
+          this.activePersons.push(person);
+        }
+      }
     }
   },
   created() {
-    this.fetchPatients();
+    this.fetchPersons();
   }
 });

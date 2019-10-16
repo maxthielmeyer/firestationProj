7a017dfe0f50b6398bf47a56b9c1bc54b8e738fc
Grace Liu <gracliu@iu.edu>
2019-10-16 10:50:24 -0400

    Update DbConnection.php
    updated DbConnection.php

--- app/class/DbConnection.php
+++ app/class/DbConnection.php
@@ -14,12 +14,12 @@ class DbConnection
       }
 
       try {
-          $dsn = 'mysql:host='.getenv('MYSQL_HOST').';dbname='.getenv['MYSQL_DATABASE'].';charset=utf8';
+          $dsn = 'mysql:host='.getenv('MYSQL_HOST').';dbname='.getenv('MYSQL_DATABASE').';charset=utf8';
           error_log($dsn);
           self::$connection = new PDO(
              $dsn,
-             getenv['MYSQL_USER'],
-             getenv['MYSQL_PASSWORD'],
+             getenv('MYSQL_USER'),
+             getenv('MYSQL_PASSWORD'),
              [
                  PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
